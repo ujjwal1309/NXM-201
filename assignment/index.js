9b1b29095ef56df4314a40037f07a0f7225f106a
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-04-23 13:07:30 +0530

    done

--- assignment/index.js
+++ assignment/index.js
@@ -3,9 +3,9 @@ const { WebSocket, WebSocketServer } = require("ws");
 const wss = new WebSocketServer({ port: 4000 });
 
 const candidates = [
-  { id: 1, name: "modi", votes: 0 },
-  { id: 2, name: "rahul", votes: 0 },
-  { id: 3, name: "yogi", votes: 0 },
+  { id: 1, name: "modi", votes: 0, voters:[] },
+  { id: 2, name: "rahul", votes: 0,voters:[] },
+  { id: 3, name: "yogi", votes: 0,voters:[] },
 ];
 
 wss.on("connection", (socket) => {
@@ -13,11 +13,26 @@ wss.on("connection", (socket) => {
 
   socket.send(JSON.stringify(candidates));
 
+  //Broadcasting the data
+
   socket.on("message", function message(data, isBinary) {
+    data=data.toString().split("|");
+    let id=+data[0],name=data[1];
+    data=data.toString();
+    candidates.forEach(el=>{
+      if(el.id===id) {
+        el.votes++
+      };
+      el.voters.push(name)
+    })
     wss.clients.forEach(function each(client) {
-      if (client !== socket && client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(candidates), { binary: isBinary });
       }
     });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A client got disconnected");
   });
 });
