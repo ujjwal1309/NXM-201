bfe3db731dd4930935af60269c870c7777c46c98
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-07-21 13:25:29 +0530

    login and signup functionalities done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -18,4 +18,5 @@ function App() {
   );
 }
 
+Allow me to introduce myself briefly. My name is [Your Full Name], and I am currently a [Grade Level] student at [Current School Name]. I have consistently demonstrated a strong passion for learning and a commitment to achieving academic excellence. Throughout my academic journey, I have actively engaged in various extracurricular activities, which have shaped me into a well-rounded individual with leadership skills and a desire to contribute positively to my community.
 export default App;
